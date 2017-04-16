f49518d2a5f1157c41a4d71d9c3b406d73e38472
Andrei Gherzan <andrei@resin.io>
2017-04-16 01:14:21 +0100

    run.sh: Add configurable parameter
    Signed-off-by: Andrei Gherzan <andrei@resin.io>

--- src/run.sh
+++ src/run.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+# Configurable parameters
+CAMERA_ROTATE=${CAMERA_ROTATE:-0}
+RTSP_PORT=${RTSP_PORT:-8555}
+FRAMERATE=${FRAMERATE:-30}
+V4L2_W=${V4L2_W:-1920}
+V4L2_H=${V4L2_H:-1080}
+
 # Get the absolute script location
 pushd `dirname $0` > /dev/null 2>&1
 SCRIPTPATH=`pwd`
@@ -18,14 +25,16 @@ if [ -n "$CAMERA_ROTATE" ]; then
 fi
 
 # Did the last run left hanging sockets?
-if [ -f "/proc/sys/net/ipv4/tcp_fin_timeout" ]; then
-    TIME_WAIT_INTERVAL=$(cat /proc/sys/net/ipv4/tcp_fin_timeout)
-else
-    TIME_WAIT_INTERVAL=60
+if [ -z "$TIME_WAIT_INTERVAL" ]; then
+    if [ -f "/proc/sys/net/ipv4/tcp_fin_timeout" ]; then
+        TIME_WAIT_INTERVAL=$(cat /proc/sys/net/ipv4/tcp_fin_timeout)
+    else
+        TIME_WAIT_INTERVAL=60
+    fi
 fi
 STARTTIME=$(date +%s)
 ENDTIME=$(date +%s)
-until [ -z "$(netstat | grep 8555 | grep TIME_WAIT)" ]; do
+until [ -z "$(netstat | grep $RTSP_PORT | grep TIME_WAIT)" ]; do
     if [ $(($ENDTIME - $STARTTIME)) -le $TIME_WAIT_INTERVAL ]; then
         log WARN "Socket is in TIME_WAIT... Waiting..."
         sleep 5
@@ -37,4 +46,4 @@ until [ -z "$(netstat | grep 8555 | grep TIME_WAIT)" ]; do
 done
 
 log "Start app..."
-/app/v4l2rtspserver/v4l2rtspserver -F 30 -W 1920 -H 1080 -P 8555 /dev/video0
+/app/v4l2rtspserver/v4l2rtspserver -F $FRAMERATE -W $V4L2_W -H $V4L2_H -P $RTSP_PORT /dev/video0
